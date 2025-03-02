9cabaf68ff37c66ab17cfd57e7820934d2aa623a
KBiz248 <129792898+KBiz248@users.noreply.github.com>
2025-03-02 00:36:30 -0500

    begin writing loop that displays the info of each item of library array on the page

--- script.js
+++ script.js
@@ -15,6 +15,33 @@ addBookToLibrary('hobbit', 'tolkien', 'many', 'yes');
 addBookToLibrary('animorphs', 'applegate', 'just a few', 'oh yeah');
 addBookToLibrary('new title', 'a person', 'no idea', 'not yet')
 
-for(let i = 0; i < library.length; i++){
-    console.log(library[i])
-}
+//for(let i = 0; i < library.length; i++){
+    //console.log(library[i])
+//  }
+
+for (const book of library){
+   
+    
+        let card = document.createElement('div');
+        card.style='display: grid; grid-template-rows: 50px 50px; grid-template-columns: 150px 150px;'
+    
+
+    {
+        let div = document.createElement('div');
+        div.textContent=book.title;
+        div.style='grid-area: 1/1/2/2'
+        card.appendChild(div);
+        document.getElementById('cardArea').appendChild(card);
+    }
+
+    {
+      let div = document.createElement('div');
+      div.textContent=book.author;
+      div.style='grid-area: 2/2/3/3'
+      document.getElementById('cardArea').appendChild(div)
+      card.appendChild(div); 
+    }
+
+   
+   
+    }
