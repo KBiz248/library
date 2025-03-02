eec067ef8b4fe143bddb64f0392db275ba486295
KBiz248 <129792898+KBiz248@users.noreply.github.com>
2025-03-01 23:35:46 -0500

    add Book() object constructor and addBookToLibrary() function

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log('hello world!')
+const library = [];
+
+function Book(title, author, pages, readStatus) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.readStatus = readStatus;
+}
+
+function addBookToLibrary(tit, auth, pag, readStat){
+    library.push(new Book(tit, auth, pag, readStat));
+}
+
+addBookToLibrary('hobbit', 'tolkien', 'many', 'yes');
+addBookToLibrary('animorphs', 'applegate', 'just a few', 'oh yeah');
+addBookToLibrary('new title', 'a person', 'no idea', 'not yet')
+
+for(let i = 0; i < library.length; i++){
+    console.log(library[i])
+}
